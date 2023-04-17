ca73ee7331daf6be7f96450c0b7eab650b1f3e35
Hyun-is <s7651111@gmail.com>
2023-04-17 14:23:17 +0900

    15

---
+++ ex1-15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* nxm개의 사각형 * 출력*/
+
+/*int main (void)
+{
+    printf("직사각형을 출력합니다.\n");
+    printf("높이 : ");
+    int h;
+    scanf("%d", &h);
+    printf("너비 : ");
+    int w;
+    scanf("%d", &w);
+    
+    for(int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < w; j++)
+            printf("*");
+        printf("\n");
+    }
+    
+    
+    return 0;
+}*/
