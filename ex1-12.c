93d192537c07cc65b70f4047ddbbb42322119e0e
Hyun-is <s7651111@gmail.com>
2023-04-17 14:21:05 +0900

    12

---
+++ ex1-12.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* 1 ~ 9 곱셈표*/
+
+/*int main (void)
+{
+    printf("   |");
+    for (int i = 1; i <= 9; i++) printf("%3d", i);
+    printf("\n---+---------------------------\n");
+    
+    for (int i = 1; i <= 9; i++)
+    {
+        printf ("%2d | ", i);
+        for (int j = 1; j <= 9; j++)
+        {
+            printf("%2d ", i * j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}*/
