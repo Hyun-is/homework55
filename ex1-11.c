e6f46837f7aeead29b854b2cfe4e7788a3388e10
Hyun-is <s7651111@gmail.com>
2023-04-17 14:19:38 +0900

    11

---
+++ ex1-11.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*양의 정수를 입력하고 자릿수를 출력하는 프로그램*/
+
+/*int main (void)
+{
+    int n;
+    int cnt = 0;
+    
+    do {
+        scanf("%d", &n);
+    }while(n < 0);
+    
+    while (n > 0)
+    {
+        n /= 10;
+        cnt++;
+    }
+    
+    printf("그 수는 %d 자리입니다.\n", cnt);
+    
+    return 0;
+
+}
+*/
